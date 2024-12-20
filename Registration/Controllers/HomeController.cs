9e8784719a3d35ddadd0e2233ac8690e8c89b917
kirtigalani <kittugalani@gmail.com>
2024-12-20 14:14:40 +0530

    Add project files.

---
+++ Registration/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
+using Registration.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index(registration register,int i=0)
+        {
+            DataSet dataset = register.Select();
+            ViewBag.data = dataset.Tables[0];
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(registration register, int id = 0)
+        {
+            register.Delete(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult update(registration register, int id = 0)
+        {
+            DataSet dataset = register.update(id);
+            ViewBag.Data = dataset.Tables[0];
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(registration register)
+        {
+            string FName = register.FName;
+            string LName = register.LName;
+            string Gender = register.Gender;
+            string city = register.city;
+            string State = register.State;
+            string mail = register.mail;
+            string DOB = register.DOB;
+            string Address = register.Address;
+
+            register.Insert(FName, LName, Gender, city, State, mail, DOB, Address);
+
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Registration/Models/registration .cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace Registration.Models
+{
+    public class registration
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Gender { get; set; }
+        public string city { get; set; }
+        public string State { get; set; }
+        public string mail { get; set; }
+        public string DOB { get; set; }
+        public string Address { get; set; }
+
+        SqlConnection sqlconnect = new SqlConnection("Data Source =.\\SQLEXPRESS;Database=registration;User Id=sa;pwd=123;Encrypt=False;TrustServerCertificate=False;");
+        
+        public int Insert(string Fname,string LName,string Gender,string city,string State,string mail, string DOB,string Address)
+        {
+            SqlCommand command = new SqlCommand("insert into [dbo].[Users](Fname,LName,Gender,city,State,mail,DOB,Address)values('" + Fname + "','" + LName + "','" + Gender + "','" + city + "','" + State + "','" + mail + "','" + DOB + "','" + Address + "')", sqlconnect);
+            sqlconnect.Open();
+            return command.ExecuteNonQuery();
+        }
+
+        public DataSet Select()
+        {
+            SqlCommand command = new SqlCommand("select * from [dbo].[Users]", sqlconnect);
+            SqlDataAdapter dataAdap = new SqlDataAdapter(command);
+            DataSet dataset = new DataSet();
+            dataAdap.Fill(dataset);
+            return dataset;
+        }
+        public int Delete(int id)
+        {
+            SqlCommand command = new SqlCommand("delete from [dbo].[Users] where id=" + id, sqlconnect);
+            sqlconnect.Open();
+            return command.ExecuteNonQuery();
+        }
+        public DataSet update(int id)
+        {
+            SqlCommand command = new SqlCommand("select * from [dbo].[Users] where id=" + id, sqlconnect);
+            SqlDataAdapter dataAdap = new SqlDataAdapter(command);
+            DataSet dataset = new DataSet();
+            dataAdap.Fill(dataset);
+            return dataset;
+        }
+
+    }
+}
